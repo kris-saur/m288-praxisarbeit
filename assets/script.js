3d68d1899071637f33719bcd9caca9630942a579
kris-saur <malgorzata.budek@sbw-media.ch>
2024-03-15 11:11:35 +0100

    we tried joki we tried

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 // Aussehen
 
-for (var i=0; i<20; i++) {
-    document.getElementById("box").innerHTML=document.getElementById("box").innerHTML+ "<div><div class='1'></div><div class='2'></div><div class='3'></div><div class='4'></div><div class='5'></div><div class='6'></div><div class='7'></div><div class='8'></div><div class='9'></div><div class='10'></div></div>";
+for (var i = 0; i < 20; i++) {
+    document.getElementById("box").innerHTML = document.getElementById("box").innerHTML + 
+    "<div><div class='1'></div><div class='2'></div><div class='3'></div><div class='4'></div><div class='5'></div><div class='6'></div><div class='7'></div><div class='8'></div><div class='9'></div><div class='10'></div></div>";
 }
 
 var x = 10;
@@ -33,16 +34,16 @@ function move(row, length, s) {
             length++;
             document.getElementsByClassName(row.toString())[s].style.backgroundColor = '';
             s++;
-        } 
+        }
         else if (length == 20) {
             length = s - 1
             s = 19;
             turn = 'backward';
-        } 
+        }
         else if (length < 20 && turn == 'backward') {
             document.getElementsByClassName(row.toString())[length].style.backgroundColor = '#95c11f';
             length--;
-            document.getElementsByClassName(row.toString())[s].style.backgroundColor='';
+            document.getElementsByClassName(row.toString())[s].style.backgroundColor = '';
             s--;
             if (length == -1) {
                 length = s + 1;
@@ -72,28 +73,33 @@ function cutting_extra(block) {
     var sum = 0;
     if (block == 10) {
         sum = 10;
-    } 
+    }
     else {
         for (var i = 0; i <= 19; i++) {
+
             elem1 = document.getElementsByClassName(block.toString())[i];
             elem2 = document.getElementsByClassName((block + 1).toString())[i];
 
-        if (window.getComputedStyle(elem1).getPropertyValue("background-color") != window.getComputedStyle(elem2).getPropertyValue("background-color")) {
-                if (window.getComputedStyle(elem2).getPropertyValue("background-color") == 'rgba(0,0,0,0)') {
-                    elem1.style.backgroundColor = elem2.style.backgroundColor;
-                }
-            } 
-            else {
-                if (window.getComputedStyle(elem2).getPropertyValue("background-color") != 'rgba(0,0,0,0)')
-                    sum++;
-            }
+            // 
+            
+            // if (
+            //     window.getComputedStyle(elem1).getPropertyValue("background-color") != 
+            //     window.getComputedStyle(elem2).getPropertyValue("background-color")) {
+            //     if (window.getComputedStyle(elem2).getPropertyValue("background-color") == 'rgba(0,0,0,0)') {
+            //         elem1.style.backgroundColor = elem2.style.backgroundColor;
+            //     }
+            // }
+            // else {
+            //     if (window.getComputedStyle(elem2).getPropertyValue("background-color") != 'rgba(0,0,0,0)')
+            //         sum++;
+            // }
         }
     }
     length = sum;
     if (length != 0) {
         score = score + 10;
         document.getElementById("score").innerHTML = score;
-    } 
+    }
     else {
         setTimeout(() => {
             alert("GAME OVER!! DEIN SCORE IST :-" + score);
